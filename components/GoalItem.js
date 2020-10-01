62ecd7cd058555129758e9ce60c1e4437c71f7b2
Sachin Singh <sachin@Sachins-iMac.local>
2020-10-01 13:10:56 +0530

    completed first react native project

---
+++ components/GoalItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const GoalItem = (props) => {
+  return (
+    <TouchableOpacity onPress={props.onDelete.bind(this, props.id)}>
+      <View style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#666",
+  },
+});
+export default GoalItem;
